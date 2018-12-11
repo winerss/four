2dcb644f5d251f4cc97e08ff273fcb4bb7cdcffd
winerss <2857505905@qq.com>
2018-12-11 22:09:41 +0800

    1211

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ export default {
   position: absolute;
   height: 100%;
   width: 100%;
-  background: url('assets/img/bg.png');
+  background: url('assets/img/bg.jpg');
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
